a32c4f72fadb041fbc2e1b16a663686232984228
Khoi Nguyen <nguyendkhoi25@gmail.com>
2025-05-21 15:43:33 +0200

    change api

--- frontEnd/src/api/index.ts
+++ frontEnd/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosError, AxiosResponse } from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "my-django-api.onrender.com/api";
 
 // 1. Tạo một instance Axios chung
 const api: AxiosInstance = axios.create({
