8bfd6262181b43add349e6fccc20438fcfae4d01
Khoi Nguyen <nguyendkhoi25@gmail.com>
2025-05-21 16:09:30 +0200

    Change setting back end

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -103,6 +103,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://aipda-fe.onrender.com"
 ]
 
 CORS_ALLOWED_METHODS = [
